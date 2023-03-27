910522a5751ce1321c2380880d433c68b314ab85
nischalkarki <74295013+A4OnE@users.noreply.github.com>
2023-03-27 12:25:18 +0545

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import HomePage from "./home/index";
+import { Inter,Raleway,Fasthand } from 'next/font/google'
+
+const inter = Raleway({ subsets: ['latin'],weight:['400'],preload:true })
 export default function Home() {
   return (
     <div>
