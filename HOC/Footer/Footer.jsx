53cdabda18d02beaf8f983c63fd59ffb574b48b7
nischal karki <nischalkarki@nischals-MacBook-Pro.local>
2023-04-09 13:01:40 +0545

    newUpload2

--- HOC/Footer/Footer.jsx
+++ HOC/Footer/Footer.jsx
@@ -4,34 +4,38 @@ import React from "react";
 import { Fade } from "react-awesome-reveal";
 import SocialMedia from "../../PageComponent/Home/SocialMedia/SocialMedia";
 import kitty from "../../UI/Resources/Image/kitty-unscreen.gif";
+import { Raleway, Fasthand } from "next/font/google";
+const inter = Fasthand({ subsets: ["latin"], weight: ["400"], preload: true });
+const inters = Raleway({ subsets: ["latin"], weight: ["400"], preload: true });
+
 function Footer() {
   return (
     <div className="  flex flex-col items-center Raleway  py-8 px-2 md:px-0">
       <div>
         <Link href={"/"}>
           <div
-            className=" h-10   flex justify-center items-center cursor-pointer mt-12
-         text-xs   md:text-xl font-bold  Agustina"
+            className={` h-10   flex justify-center items-center cursor-pointer mt-12
+         text-xs   md:text-xl font-bold  ${inter.className} `}
           >
             <div className=" flex gap-2 md:gap-4   ">
-              <Fade delay={0e3} cascade damping={0.1}>
+              <Fade delay={0e3} triggerOnce cascade damping={0.1}>
                 {" "}
                 <div className=" hidden md:block">😈 🎃 😈</div>
                 {/* <div className=" block md:hidden">😈</div> */}
               </Fade>
               <div className="text-xs md:text-2xl">
-                <Fade delay={1e3} cascade damping={0.1}>
+                <Fade delay={1e3} triggerOnce cascade damping={0.1}>
                   Made by Nischal Karki chhetri
                 </Fade>
               </div>
-              <Fade delay={4e3} cascade damping={0.1}>
+              <Fade delay={4e3} triggerOnce cascade damping={0.1}>
                 {" "}
                 <div className=" hidden md:block">😈 🎃 😈</div>
                 {/* <div className=" block md:hidden">😈</div> */}
               </Fade>
             </div>
             <div className="w-64 h-64 bg-red-800">
-              <Fade delay={5e3} cascade damping={0.1}>
+              <Fade delay={5e3} triggerOnce cascade damping={0.1}>
                 <div className="w-64 h-64  flex  items-center">
                   <Image
                     src={kitty}
@@ -47,7 +51,9 @@ function Footer() {
         </Link>
       </div>
 
-      <div className=" py-4 md:flex justify-between    w-full items-end    ">
+      <div
+        className={`py-4 md:flex justify-between    w-full items-end ${inters.className}`}
+      >
         <div className="capitalize font-light text-sm md:text-[14px]   md:flex justify-center ">
           &copy; {new Date().getFullYear()} Nischal Karki Chhetri. All Rights
           Reserved.{" "}
